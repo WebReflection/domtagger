acbfc7eb03eed32fe56a22d688506c7c6c7853b1
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-11-29 12:05:41 +0100

    fixed specific MS Edge issue

--- esm/constants.js
+++ esm/constants.js
@@ -1,6 +1,7 @@
 // Custom
 var NOT_IE = 'content' in document.createElement('template');
-var UID = (NOT_IE ? '-0' : '_dt: ') + Math.random() + (NOT_IE ? '0%' : ';');
+var UID = (NOT_IE ? '-' : '_dt: ') + Math.random().toFixed(6) + (NOT_IE ? '0%' : ';');
+//                                              ^ Edge issue ^
 var UIDC = '<!--' + UID + '-->';
 
 // DOM
--- index.js
+++ index.js
@@ -137,7 +137,8 @@ var domtagger = (function (document) {
 
   // Custom
   var NOT_IE = 'content' in document.createElement('template');
-  var UID = (NOT_IE ? '-0' : '_dt: ') + Math.random() + (NOT_IE ? '0%' : ';');
+  var UID = (NOT_IE ? '-' : '_dt: ') + Math.random().toFixed(6) + (NOT_IE ? '0%' : ';');
+  //                                              ^ Edge issue ^
   var UIDC = '<!--' + UID + '-->';
 
   // DOM
