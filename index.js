1a9e98d3e16291c9cbecd262829a61c4be0317aa
Jimmy Wärting <jimmy@warting.se>
2023-04-05 16:27:33 +0200

    remove trim and weakmap polyfill

--- index.js
+++ index.js
@@ -2,57 +2,6 @@ var domtagger = (function (document) {
   'use strict';
 
   /*! (c) Andrea Giammarchi - ISC */
-  var self = {};
-
-  try {
-    self.WeakMap = WeakMap;
-  } catch (WeakMap) {
-    // this could be better but 90% of the time
-    // it's everything developers need as fallback
-    self.WeakMap = function (id, Object) {
-
-      var dP = Object.defineProperty;
-      var hOP = Object.hasOwnProperty;
-      var proto = WeakMap.prototype;
-
-      proto["delete"] = function (key) {
-        return this.has(key) && delete key[this._];
-      };
-
-      proto.get = function (key) {
-        return this.has(key) ? key[this._] : void 0;
-      };
-
-      proto.has = function (key) {
-        return hOP.call(key, this._);
-      };
-
-      proto.set = function (key, value) {
-        dP(key, this._, {
-          configurable: true,
-          value: value
-        });
-        return this;
-      };
-
-      return WeakMap;
-
-      function WeakMap(iterable) {
-        dP(this, '_', {
-          value: '_@ungap/weakmap' + id++
-        });
-        if (iterable) iterable.forEach(add, this);
-      }
-
-      function add(pair) {
-        this.set(pair[0], pair[1]);
-      }
-    }(Math.random(), Object);
-  }
-
-  var WeakMap$1 = self.WeakMap;
-
-  /*! (c) Andrea Giammarchi - ISC */
   var createContent = function (document) {
 
     var FRAGMENT = 'fragment';
@@ -66,7 +15,6 @@ var domtagger = (function (document) {
       var content = create(FRAGMENT);
       var template = create(TEMPLATE);
       var childNodes = null;
-
       if (/^[^\S]*?<(col(?:group)?|t(?:head|body|foot|r|d|h))/i.test(html)) {
         var selector = RegExp.$1;
         template.innerHTML = '<table>' + html + '</table>';
@@ -75,29 +23,23 @@ var domtagger = (function (document) {
         template.innerHTML = html;
         childNodes = template.childNodes;
       }
-
       append(content, childNodes);
       return content;
     };
     return function createContent(markup, type) {
       return (type === 'svg' ? createSVG : createHTML)(markup);
     };
-
     function append(root, childNodes) {
       var length = childNodes.length;
-
-      while (length--) {
-        root.appendChild(childNodes[0]);
-      }
+      while (length--) root.appendChild(childNodes[0]);
     }
-
     function create(element) {
       return element === FRAGMENT ? document.createDocumentFragment() : document.createElementNS('http://www.w3.org/1999/xhtml', element);
-    } // it could use createElementNS when hasNode is there
+    }
+
+    // it could use createElementNS when hasNode is there
     // but this fallback is equally fast and easier to maintain
     // it is also battle tested already in all IE
-
-
     function createSVG(svg) {
       var content = create(FRAGMENT);
       var template = create('div');
@@ -109,45 +51,34 @@ var domtagger = (function (document) {
 
   /*! (c) Andrea Giammarchi - ISC */
   var importNode = function (document, appendChild, cloneNode, createTextNode, importNode) {
-    var _native = (importNode in document); // IE 11 has problems with cloning templates:
+    var _native = (importNode in document);
+    // IE 11 has problems with cloning templates:
     // it "forgets" empty childNodes. This feature-detects that.
-
-
     var fragment = document.createDocumentFragment();
     fragment[appendChild](document[createTextNode]('g'));
     fragment[appendChild](document[createTextNode](''));
     /* istanbul ignore next */
-
     var content = _native ? document[importNode](fragment, true) : fragment[cloneNode](true);
     return content.childNodes.length < 2 ? function importNode(node, deep) {
       var clone = node[cloneNode]();
-
-      for (var
-      /* istanbul ignore next */
-      childNodes = node.childNodes || [], length = childNodes.length, i = 0; deep && i < length; i++) {
+      for (var /* istanbul ignore next */
+        childNodes = node.childNodes || [], length = childNodes.length, i = 0; deep && i < length; i++) {
         clone[appendChild](importNode(childNodes[i], deep));
       }
-
       return clone;
-    } :
-    /* istanbul ignore next */
+    } : /* istanbul ignore next */
     _native ? document[importNode] : function (node, deep) {
       return node[cloneNode](!!deep);
     };
   }(document, 'appendChild', 'cloneNode', 'createTextNode', 'importNode');
 
-  var trim = ''.trim ||
-  /* istanbul ignore next */
-  function () {
-    return String(this).replace(/^\s+|\s+/g, '');
-  };
-
   /*! (c) Andrea Giammarchi - ISC */
+
   // Custom
-  var UID = '-' + Math.random().toFixed(6) + '%'; //                           Edge issue!
+  var UID = '-' + Math.random().toFixed(6) + '%';
+  //                           Edge issue!
 
   var UID_IE = false;
-
   try {
     if (!function (template, content, tabindex) {
       return content in template && (template.innerHTML = '<p ' + tabindex + '="' + UID + '"></p>', template[content].childNodes[0].getAttribute(tabindex) == UID);
@@ -156,9 +87,9 @@ var domtagger = (function (document) {
       UID_IE = true;
     }
   } catch (meh) {}
+  var UIDC = '<!--' + UID + '-->';
 
-  var UIDC = '<!--' + UID + '-->'; // DOM
-
+  // DOM
   var COMMENT_NODE = 8;
   var ELEMENT_NODE = 1;
   var TEXT_NODE = 3;
@@ -177,15 +108,12 @@ var domtagger = (function (document) {
   var attrSeeker = new RegExp(tagName + attrName + attrPartials + '+)([' + spaces + ']*/?>)', 'g');
   var selfClosing = new RegExp(tagName + attrName + attrPartials + '*)([' + spaces + ']*/>)', 'g');
   var findAttributes = new RegExp('(' + attrName + '\\s*=\\s*)([\'"]?)' + UIDC + '\\2', 'gi');
-
   function attrReplacer($0, $1, $2, $3) {
     return '<' + $1 + $2.replace(findAttributes, replaceAttributes) + $3;
   }
-
   function replaceAttributes($0, $1, $2) {
     return $1 + ($2 || '"') + UID + ($2 || '"');
   }
-
   function fullClosing($0, $1, $2) {
     return VOID_ELEMENTS.test($1) ? $0 : '<' + $1 + $2 + '></' + $1 + '>';
   }
@@ -206,8 +134,11 @@ var domtagger = (function (document) {
     };
   });
 
-  /* istanbul ignore next */
+  var trim = ''.trim || /* istanbul ignore next */function () {
+    return String(this).replace(/^\s+|\s+/g, '');
+  };
 
+  /* istanbul ignore next */
   var normalizeAttributes = UID_IE ? function (attributes, parts) {
     var html = parts.join(' ');
     return parts.slice.call(attributes, 0).sort(function (left, right) {
@@ -216,39 +147,30 @@ var domtagger = (function (document) {
   } : function (attributes, parts) {
     return parts.slice.call(attributes, 0);
   };
-
   function find(node, path) {
     var length = path.length;
     var i = 0;
-
-    while (i < length) {
-      node = node.childNodes[path[i++]];
-    }
-
+    while (i < length) node = node.childNodes[path[i++]];
     return node;
   }
-
   function parse(node, holes, parts, path) {
     var childNodes = node.childNodes;
     var length = childNodes.length;
     var i = 0;
-
     while (i < length) {
       var child = childNodes[i];
-
       switch (child.nodeType) {
         case ELEMENT_NODE:
           var childPath = path.concat(i);
           parseAttributes(child, holes, parts, childPath);
           parse(child, holes, parts, childPath);
           break;
-
         case COMMENT_NODE:
           var textContent = child.textContent;
-
           if (textContent === UID) {
             parts.shift();
-            holes.push( // basicHTML or other non standard engines
+            holes.push(
+            // basicHTML or other non standard engines
             // might end up having comments in nodes
             // where they shouldn't, hence this check.
             SHOULD_USE_TEXT_CONTENT.test(node.nodeName) ? Text(node, path) : Any(child, path.concat(i)));
@@ -256,7 +178,6 @@ var domtagger = (function (document) {
             switch (textContent.slice(0, 2)) {
               case '/*':
                 if (textContent.slice(-2) !== '*/') break;
-
               case "\uD83D\uDC7B":
                 // ghost
                 node.removeChild(child);
@@ -264,28 +185,22 @@ var domtagger = (function (document) {
                 length--;
             }
           }
-
           break;
-
         case TEXT_NODE:
           // the following ignore is actually covered by browsers
           // only basicHTML ends up on previous COMMENT_NODE case
           // instead of TEXT_NODE because it knows nothing about
           // special style or textarea behavior
-
           /* istanbul ignore if */
           if (SHOULD_USE_TEXT_CONTENT.test(node.nodeName) && trim.call(child.textContent) === UIDC) {
             parts.shift();
             holes.push(Text(node, path));
           }
-
           break;
       }
-
       i++;
     }
   }
-
   function parseAttributes(node, holes, parts, path) {
     var attributes = node.attributes;
     var cache = [];
@@ -293,66 +208,54 @@ var domtagger = (function (document) {
     var array = normalizeAttributes(attributes, parts);
     var length = array.length;
     var i = 0;
-
     while (i < length) {
       var attribute = array[i++];
       var direct = attribute.value === UID;
       var sparse;
-
       if (direct || 1 < (sparse = attribute.value.split(UIDC)).length) {
-        var name = attribute.name; // the following ignore is covered by IE
+        var name = attribute.name;
+        // the following ignore is covered by IE
         // and the IE9 double viewBox test
-
         /* istanbul ignore else */
-
         if (cache.indexOf(name) < 0) {
           cache.push(name);
           var realName = parts.shift().replace(direct ? /^(?:|[\S\s]*?\s)(\S+?)\s*=\s*('|")?$/ : new RegExp('^(?:|[\\S\\s]*?\\s)(' + name + ')\\s*=\\s*(\'|")[\\S\\s]*', 'i'), '$1');
-          var value = attributes[realName] || // the following ignore is covered by browsers
+          var value = attributes[realName] ||
+          // the following ignore is covered by browsers
           // while basicHTML is already case-sensitive
-
           /* istanbul ignore next */
           attributes[realName.toLowerCase()];
           if (direct) holes.push(Attr(value, path, realName, null));else {
             var skip = sparse.length - 2;
-
-            while (skip--) {
-              parts.shift();
-            }
-
+            while (skip--) parts.shift();
             holes.push(Attr(value, path, realName, sparse));
           }
         }
-
         remove.push(attribute);
       }
     }
-
     length = remove.length;
     i = 0;
+
     /* istanbul ignore next */
-
     var cleanValue = 0 < length && UID_IE && !('ownerSVGElement' in node);
-
     while (i < length) {
       // Edge HTML bug #16878726
-      var attr = remove[i++]; // IE/Edge bug lighterhtml#63 - clean the value or it'll persist
-
+      var attr = remove[i++];
+      // IE/Edge bug lighterhtml#63 - clean the value or it'll persist
       /* istanbul ignore next */
-
-      if (cleanValue) attr.value = ''; // IE/Edge bug lighterhtml#64 - don't use removeAttributeNode
-
+      if (cleanValue) attr.value = '';
+      // IE/Edge bug lighterhtml#64 - don't use removeAttributeNode
       node.removeAttribute(attr.name);
-    } // This is a very specific Firefox/Safari issue
+    }
+
+    // This is a very specific Firefox/Safari issue
     // but since it should be a not so common pattern,
     // it's probably worth patching regardless.
     // Basically, scripts created through strings are death.
     // You need to create fresh new scripts instead.
     // TODO: is there any other node that needs such nonsense?
-
-
     var nodeName = node.nodeName;
-
     if (/^script$/i.test(nodeName)) {
       // this used to be like that
       // var script = createElement(node, nodeName);
@@ -362,16 +265,11 @@ var domtagger = (function (document) {
       var script = document.createElement(nodeName);
       length = attributes.length;
       i = 0;
-
-      while (i < length) {
-        script.setAttributeNode(attributes[i++].cloneNode(true));
-      }
-
+      while (i < length) script.setAttributeNode(attributes[i++].cloneNode(true));
       script.textContent = node.textContent;
       node.parentNode.replaceChild(script, node);
     }
   }
-
   function Any(node, path) {
     return {
       type: 'any',
@@ -379,7 +277,6 @@ var domtagger = (function (document) {
       path: path
     };
   }
-
   function Attr(node, path, name, sparse) {
     return {
       type: 'attr',
@@ -389,7 +286,6 @@ var domtagger = (function (document) {
       sparse: sparse
     };
   }
-
   function Text(node, path) {
     return {
       type: 'text',
@@ -398,9 +294,8 @@ var domtagger = (function (document) {
     };
   }
 
-  // globals
-  var parsed = umap(new WeakMap$1());
-
+  // utils
+  var parsed = umap(new WeakMap());
   function createInfo(options, template) {
     var markup = (options.convert || sanitize)(template);
     var transform = options.transform;
@@ -416,11 +311,9 @@ var domtagger = (function (document) {
         var len = holes.length;
         var i = 0;
         var off = 0;
-
         while (i < len) {
           var info = holes[i++];
           var node = find(content, info.path);
-
           switch (info.type) {
             case 'any':
               updates.push({
@@ -428,7 +321,6 @@ var domtagger = (function (document) {
                 sparse: false
               });
               break;
-
             case 'attr':
               var sparse = info.sparse;
               var fn = options.attribute(node, info.name, info.node);
@@ -444,7 +336,6 @@ var domtagger = (function (document) {
                 });
               }
               break;
-
             case 'text':
               updates.push({
                 fn: options.text(node),
@@ -454,49 +345,36 @@ var domtagger = (function (document) {
               break;
           }
         }
-
         len += off;
         return function () {
           var length = arguments.length;
-
           if (len !== length - 1) {
             throw new Error(length - 1 + ' values instead of ' + len + '\n' + template.join('${value}'));
           }
-
           var i = 1;
           var off = 1;
-
           while (i < length) {
             var update = updates[i - off];
-
             if (update.sparse) {
               var values = update.values;
               var value = values[0];
               var j = 1;
               var l = values.length;
               off += l - 2;
-
-              while (j < l) {
-                value += arguments[i++] + values[j++];
-              }
-
+              while (j < l) value += arguments[i++] + values[j++];
               update.fn(value);
             } else update.fn(arguments[i++]);
           }
-
           return content;
         };
       }
     };
   }
-
   function createDetails(options, template) {
     var info = parsed.get(template) || parsed.set(template, createInfo(options, template));
     return info.updates(importNode.call(document, info.content, true));
   }
-
   var empty = [];
-
   function domtagger(options) {
     var previous = empty;
     var updates = cleanContent;
@@ -505,15 +383,12 @@ var domtagger = (function (document) {
       return updates.apply(null, arguments);
     };
   }
-
   function cleanContent(fragment) {
     var childNodes = fragment.childNodes;
     var i = childNodes.length;
-
     while (i--) {
       var child = childNodes[i];
-
-      if (child.nodeType !== 1 && trim.call(child.textContent).length === 0) {
+      if (child.nodeType !== 1 && child.textContent.trim().length === 0) {
         fragment.removeChild(child);
       }
     }
@@ -521,6 +396,8 @@ var domtagger = (function (document) {
 
   
 
+  Object.defineProperty(
+
   return domtagger;
 
-}(document));
+})(document);
